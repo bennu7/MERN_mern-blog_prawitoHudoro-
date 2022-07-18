1a534eda3f17e9160f56ab6e5a91ea770fd5e640
bennu7 <laluibnuhidayatullah@gmail.com>
2022-07-19 05:56:21 +0700

    06 bacis component hooks & slicing rgister

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import { Button, Gap, Input, Link } from "../../components";
+import "./register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt="RegisterBg" />
+      </div>
+
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="input your name" />
+        <Gap height={5} />
+        <Input label="Email" placeholder="input your email" />
+        <Gap height={5} />
+        <Input label="Password" placeholder="input your password" />
+        <Gap height={50} />
+        <Button title="register" />
+        <Gap height={100} />
+        <Link title={"Kembali ke Login"} />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
