b7e81676e139552191feb41f1e564c778dbd6f85
bennu7 <laluibnuhidayatullah@gmail.com>
2022-07-23 22:37:08 +0700

    31 - connect mongodb

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const logger = require("morgan");
 
 const indexRouter = require("./src/routes/index");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -28,5 +29,17 @@ app.use((error, req, res, next) => {
     data: data,
   });
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connect to mongodb");
+  })
+  .catch((err) => {
+    console.log("failed connect to mongodb : ", err);
+  });
 
 module.exports = app;
