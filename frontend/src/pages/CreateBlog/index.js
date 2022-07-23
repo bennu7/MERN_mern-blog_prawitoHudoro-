79fc3af3def21966bf4286345e94c29f26bdf29c
bennu7 <laluibnuhidayatullah@gmail.com>
2022-07-23 16:40:37 +0700

    11 - Membuat Styling Halaman Create Blog Post

--- frontend/src/pages/CreateBlog/index.js
+++ frontend/src/pages/CreateBlog/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./createBlog.scss";
+import { Button, Gap, Input, TextArea, Upload } from "../../components";
 
 const CreateBlog = () => {
   return (
-    <div>
-      <p>CreateBlog</p>
+    <div className="blog-post">
+      <p className="title">CreateBlog new blog post</p>
+      <Input label={"Post Title"} />
+      <p>Upload Image</p>
+      <Upload />
+      <TextArea />
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title={"Save"} />
+      </div>
     </div>
   );
 };
