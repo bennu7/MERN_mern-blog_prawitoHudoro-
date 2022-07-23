7e515ea9a0c30fd487eca7affab3136633334a36
bennu7 <laluibnuhidayatullah@gmail.com>
2022-07-23 19:06:19 +0700

    handle CORS

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const userRoute = require("./users");
 const productRoute = require("./product");
 
+// handle untuk error CORS
+router.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 router.use("/api/v1/users", userRoute);
 router.use("/api/v1/products", productRoute);
 
